70b9bb11e3238e6cf42c5b1a17aaf4ffc5ef60ee
khushiiii29 <khushichoudhary7799@gmail.com>
2026-09-06 16:43:45 +0530

    Refactor server.js to simplify request handling

--- Web Assignment/Web Assignment 1/server.js
+++ Web Assignment/Web Assignment 1/server.js
@@ -1,47 +1,22 @@
-const http = require('http');
-
-const port = 3000;
-
-console.log('Server Started');
-console.log('Server starting...');
-
-const server = http.createServer((request, response) => {
-  console.log(`Request received: ${request.url}`);
-
-  let statusCode = 200;
-  let message;
-
-  switch (request.url) {
-    case '/':
-      message = 'Welcome to Smart Utility Toolkit';
-      break;
-    case '/about':
-      message = 'This is the About Page';
-      break;
-    case '/contact':
-      message = 'This is the Contact Page';
-      break;
-    default:
-      statusCode = 404;
-      message = '404 - Page Not Found';
-  }
-
-  response.writeHead(statusCode, { 'Content-Type': 'text/plain' });
-  response.end(message, () => {
-    console.log('Response sent');
-  });
-});
-
-server.on('error', (error) => {
-  if (error.code === 'EADDRINUSE') {
-    console.log(`Error: Port ${port} is already in use. Stop the other server and try again.`);
-  } else {
-    console.log(`Server error: ${error.message}`);
-  }
-
-  process.exitCode = 1;
-});
-
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+const http= require("http")
+ const server = http.createServer((req,res)=>{
+    if(req.url=="/"){
+        res.write("hello world")
+        res.end()
+    }   
+    else if(req.url=="/about"){
+        res.write("about page")
+        res.end()
+    }
+    else if(req.url=="/contact"){
+        res.write("contact page")
+        res.end()
+    }
+    else{
+        res.write("404 page not found")
+        res.end()
+    }
+ })
+ server.listen(3000,()=>{
+    console.log("server is running on port:3000")
+ })
