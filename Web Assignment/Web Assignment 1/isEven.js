5c5b05d454ab8d1f1b4b3ef895a31b0562496dc5
khushiiii29 <khushichoudhary7799@gmail.com>
2026-09-06 16:48:04 +0530

    Delete Web Assignment/Web Assignment 1/isEven.js

--- Web Assignment/Web Assignment 1/isEven.js
+++
@@ -1,9 +0,0 @@
-function isEven(number) {
-  if (typeof number !== 'number' || !Number.isFinite(number)) {
-    throw new Error('isEven requires a valid number.');
-  }
-
-  return number % 2 === 0;
-}
-
-module.exports = isEven;
